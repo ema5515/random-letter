f57a51de49d4f036766f44a8b5440b13dbe039c5
Emanuele D'Agata <ema.dagata@gmail.com>
2022-04-12 17:24:19 +0200

    20220412

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I","J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+var colors = ["#99c24d", "#f18f01", "#9368b7", "#208AAe"]
 var stop = 0;
 var i = 0;
 
-$(".start").click(function(){
-    $("body").addClass("stop");
-    $("body").removeClass("start");
-        stop = 1;
-        animation();
 
-});
+function start(){
+    $(".start-button").addClass("display-no");
+    changeColor();
+    stop = 1;
+    animation();
 
-$(".stop").click(function(){
-    stop = 0;
-    $("body").addClass("start");
-    $("body").removeClass("stop");
+    setTimeout(function(){
+        $(document).click(function(){
+            stop = 0;
+            $(document).unbind();
+            $(".start-button").removeClass("display-no");
+         });
+    }, 100);
     
-        stop = 0;
-});
-
-
+}
 function animation(){
     
     $(".letter").text(letters[i]);
@@ -35,4 +35,15 @@ function animation(){
             return;
         }
     }, 50);
+}
+
+function changeColor(){
+    var x = Math.floor(Math.random() * 4);
+    $("body").css("background-color", colors[x]);
+    var y = Math.floor(Math.random() * 4);
+    if(x===y){
+        changeColor();
+    } else{
+        $(".start-button").css("background-color", colors[y]);
+    }
 }
